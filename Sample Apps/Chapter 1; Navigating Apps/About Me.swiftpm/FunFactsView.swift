63c01948dfca6a3532dc271667e8568716fc2c71
Lorenzo Mazzarotto <mazzaos@icloud.com>
2022-05-28 03:25:30 +0200

    Fun Facts Tab
    Make your app interactive by adding a button that displays a random fact about you.

---
+++ Sample Apps/Chapter 1; Navigating Apps/About Me.swiftpm/FunFactsView.swift
@@ -0,0 +1,36 @@
+//
+//  FunFactsView.swift
+//  About Me
+//
+//  Created by Lorenzo Lins Mazzarotto on 28/05/22.
+//
+
+import SwiftUI
+
+struct FunFactsView: View {
+
+    @State private var funFact = ""
+    var body: some View {
+        VStack {
+            Text("Fun Facts")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+
+            Text(funFact)
+                .padding()
+                .font(.title)
+                .frame(minHeight: 400)
+
+            Button("Show Random Fact") {
+                funFact = information.funFacts.randomElement()!
+            }
+        }
+        .padding()
+    }
+}
+
+struct FunFactsView_Previews: PreviewProvider {
+    static var previews: some View {
+        FunFactsView()
+    }
+}
