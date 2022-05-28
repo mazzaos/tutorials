cbaea8bac65f15e255b6282ac5441720f2bb6550
Lorenzo Mazzarotto <mazzaos@icloud.com>
2022-05-28 03:23:52 +0200

    Story Tab
    Learn how to use a scroll view to create a scrolling narrative.

---
+++ Sample Apps/Chapter 1; Navigating Apps/About Me.swiftpm/StoryView.swift
@@ -0,0 +1,32 @@
+//
+//  StoryView.swift
+//  About Me
+//
+//  Created by Lorenzo Lins Mazzarotto on 28/05/22.
+//
+
+import SwiftUI
+
+struct StoryView: View {
+    var body: some View {
+        VStack {
+            Text("My Story")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding()
+
+            ScrollView {
+                Text(information.story)
+                    .font(.body)
+                    .padding()
+            }
+        }
+        .padding([.top, .bottom], 50)
+    }
+}
+
+struct StoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        StoryView()
+    }
+}
