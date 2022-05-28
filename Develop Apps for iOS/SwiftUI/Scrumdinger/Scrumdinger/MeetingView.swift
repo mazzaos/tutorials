34fb6fdcdd830f3a5f228bf2d52d7259fe800322
Lorenzo Mazzarotto <mazzaos@icloud.com>
2022-05-29 01:45:56 +0200

    Build Groups of Views
    Views define a piece of your UI. They’re the building blocks of your app. You build a complex view by composing it of small, simple views. In this section, you’ll build the header of the timer screen to display the elapsed time and remaining time of a meeting.

---
+++ Develop Apps for iOS/SwiftUI/Scrumdinger/Scrumdinger/MeetingView.swift
@@ -0,0 +1,32 @@
+//
+//  MeetingView.swift
+//  Scrumdinger
+//
+//  Created by Lorenzo Lins Mazzarotto on 29/05/22.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack {
+                    Text("Seconds Elapsed")
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                VStack {
+                    Text("Seconds Remaining")
+                    Label("600", systemImage: "hourglass.tophalf.fill")
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
