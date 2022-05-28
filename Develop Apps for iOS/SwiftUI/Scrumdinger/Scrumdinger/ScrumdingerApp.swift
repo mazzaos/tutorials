f85ac5d6234f3d8c5258a08084caea80c08fd2a7
Lorenzo Mazzarotto <mazzaos@icloud.com>
2022-05-29 01:41:16 +0200

    Create a Project
    Throughout this module, you’ll learn the essentials of app development by creating Scrumdinger. You’ll explore the fundamentals of Xcode and SwiftUI as you add new features to the app. In this section, you’ll create the Xcode project for Scrumdinger.

---
+++ Develop Apps for iOS/SwiftUI/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Lorenzo Lins Mazzarotto on 29/05/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
