cf36310201ef5f225e3500d22b64fe073f8ea92b
Lorenzo Mazzarotto <mazzaos@icloud.com>
2022-05-29 06:28:22 +0200

    Adding an Image to the Gallery
    Explore how to add and edit photos in the gallery.

--- Exploring SwiftUI Sample Apps/Chapter 2; Presenting Content/Image Gallery.swiftpm/GridView.swift
+++ Exploring SwiftUI Sample Apps/Chapter 2; Presenting Content/Image Gallery.swiftpm/GridView.swift
@@ -11,6 +11,8 @@ struct GridView: View {
     @EnvironmentObject var dataModel: DataModel
 
     private static let initialColumns = 3
+    @State private var isAddingPhoto = false
+    @State private var isEditing = false
 
     @State private var gridColumns = Array(repeating: GridItem(.flexible()), count: initialColumns)
     @State private var numColumns = initialColumns
@@ -35,6 +37,21 @@ struct GridView: View {
                         }
                         .cornerRadius(8.0)
                         .aspectRatio(1, contentMode: .fit)
+                        .overlay(alignment: .topTrailing) {
+                            if isEditing {
+                                Button {
+                                    withAnimation {
+                                        dataModel.removeItem(item)
+                                    }
+                                } label: {
+                                    Image(systemName: "xmark.square.fill")
+                                        .font(Font.title)
+                                        .symbolRenderingMode(.palette)
+                                        .foregroundStyle(.white, .red)
+                                }
+                                .offset(x: 7, y: -7)
+                            }
+                        }
                     }
                 }
                 .padding()
@@ -42,6 +59,24 @@ struct GridView: View {
         }
         .navigationBarTitle("Image Gallery")
         .navigationBarTitleDisplayMode(.inline)
+        .sheet(isPresented: $isAddingPhoto) {
+            PhotoPicker()
+        }
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button(isEditing ? "Done" : "Edit") {
+                    withAnimation { isEditing.toggle() }
+                }
+            }
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button {
+                    isAddingPhoto = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+                .disabled(isEditing)
+            }
+        }
     }
 }
 
