975e9988856b3c24dbb1d23c601ca3fb84a22960
Lorenzo Mazzarotto <mazzaos@icloud.com>
2022-05-30 02:54:43 +0200

    Fetching Panda Data
    To fetch these images and their metadata, you’ll use an observable object that fetches data asynchronously. You’ll learn more about how to structure asynchronous functions in this walkthrough.

---
+++ Exploring SwiftUI Sample Apps/Chapter 3; Retrieving Content from a Server/Meme Creator.swiftpm/PandaCollectionFetcher.swift
@@ -0,0 +1,33 @@
+//
+//  PandaCollectionFetcher.swift
+//  Meme Creator
+//
+//  Created by Lorenzo Lins Mazzarotto on 30/05/22.
+//
+
+import SwiftUI
+
+@MainActor
+class PandaCollectionFetcher: ObservableObject {
+    @Published var imageData = PandaCollection(sample: [Panda.defaultPanda])
+    @Published var currentPanda = Panda.defaultPanda
+
+    let urlString = "http://playgrounds-cdn.apple.com/assets/pandaData.json"
+
+    enum FetchError: Error {
+        case badRequest
+        case badJSON
+    }
+
+    @available(iOS 15.0, *)
+    func fetchData() async
+    throws  {
+        guard let url = URL(string: urlString) else { return }
+
+        let (data, response) = try await URLSession.shared.data(for: URLRequest(url: url))
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else { throw FetchError.badRequest }
+
+        imageData = try JSONDecoder().decode(PandaCollection.self, from: data)
+    }
+
+}
