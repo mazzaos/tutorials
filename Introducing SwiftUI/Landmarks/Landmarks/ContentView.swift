ded84c8b5ebb150e1a06f36b9ed74d1f29dc8ee2
Lorenzo Mazzarotto <mazzaos@icloud.com>
2022-05-30 12:13:04 +0200

    Initial Commit

---
+++ Introducing SwiftUI/Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Lorenzo Lins Mazzarotto on 30/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello SwiftUI!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
