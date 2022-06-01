d1fe224a1d97cfb9cfc6fb9884eb6a00fcce00c5
Lorenzo Mazzarotto <mazzaos@icloud.com>
2022-06-01 06:11:55 +0200

    Orientation Data View
    Learn to display motion data as string values in the view.

---
+++ Exploring SwiftUI Sample Apps/Chapter 4; Responding to User Input/Bubble Level.swiftpm/OrientationDataView.swift
@@ -0,0 +1,38 @@
+//
+//  OrientationDataView.swift
+//  Bubble Level
+//
+//  Created by Lorenzo Lins Mazzarotto on 01/06/22.
+//
+
+import SwiftUI
+
+struct OrientationDataView: View {
+    @EnvironmentObject var detector: MotionDetector
+
+    var rollString: String {
+        detector.roll.describeAsFixedLengthString()
+    }
+
+    var pitchString: String {
+        detector.pitch.describeAsFixedLengthString()
+    }
+
+    var body: some View {
+        VStack {
+            Text("Horizontal: " + rollString)
+                .font(.system(.body, design: .monospaced))
+            Text("Vertical: " + pitchString)
+                .font(.system(.body, design: .monospaced))
+        }
+    }
+}
+
+struct OrientationDataView_Previews: PreviewProvider {
+    @StateObject static private var motionDetector = MotionDetector(updateInterval: 0.01).started()
+
+    static var previews: some View {
+        OrientationDataView()
+            .environmentObject(motionDetector)
+    }
+}
