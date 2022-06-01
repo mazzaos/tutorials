3eb2ef88014437311bc5898f072f2fca5755a804
Lorenzo Mazzarotto <mazzaos@icloud.com>
2022-06-01 06:49:36 +0200

    Double Extension
    Investigate using an extension on Double to format text in an easy to read way.

---
+++ Exploring SwiftUI Sample Apps/Chapter 4; Responding to User Input/Seismometer.swiftpm/DoubleExtension.swift
@@ -0,0 +1,20 @@
+//
+//  DoubleExtension.swift
+//  Seismometer
+//
+//  Created by Lorenzo Lins Mazzarotto on 01/06/22.
+//
+
+import Foundation
+
+extension Double {
+    func describeAsFixedLengthString(integerDigits: Int = 2, fractionDigits: Int = 2) -> String {
+        self.formatted(
+            .number
+                .sign(strategy: .always())
+                .precision(
+                    .integerAndFractionLength(integer: integerDigits, fraction: fractionDigits)
+                )
+        )
+    }
+}
