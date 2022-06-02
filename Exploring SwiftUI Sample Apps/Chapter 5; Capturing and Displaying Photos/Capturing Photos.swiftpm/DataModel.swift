fea507237f514f2e4bfc6df41a6510f7a46c35e2
Lorenzo Mazzarotto <mazzaos@icloud.com>
2022-06-02 09:11:34 +0200

    Processing and Saving a Photo
    Discover how to unpack a captured photo and save it to your photo library.

--- Exploring SwiftUI Sample Apps/Chapter 5; Capturing and Displaying Photos/Capturing Photos.swiftpm/DataModel.swift
+++ Exploring SwiftUI Sample Apps/Chapter 5; Capturing and Displaying Photos/Capturing Photos.swiftpm/DataModel.swift
@@ -21,6 +21,10 @@ final class DataModel: ObservableObject {
         Task {
             await handleCameraPreviews()
         }
+
+        Task {
+            await handleCameraPhotos()
+        }
     }
 
     func handleCameraPreviews() async {
@@ -30,6 +34,46 @@ final class DataModel: ObservableObject {
         for await image in imageStream {
             Task { @MainActor in
                 viewfinderImage = image
+            }
+        }
+    }
+
+    func handleCameraPhotos() async {
+        let unpackedPhotoStream = camera.photoStream
+            .compactMap { self.unpackPhoto($0) }
+
+        for await photoData in unpackedPhotoStream {
+            Task { @MainActor in
+                thumbnailImage = photoData.thumbnailImage
+            }
+            savePhoto(imageData: photoData.imageData)
+        }
+    }
+
+    private func unpackPhoto(_ photo: AVCapturePhoto) -> PhotoData? {
+        guard let imageData = photo.fileDataRepresentation() else { return nil }
+
+        guard let previewCGImage = photo.previewCGImageRepresentation(),
+              let metadataOrientation = photo.metadata[String(kCGImagePropertyOrientation)] as? UInt32,
+              let cgImageOrientation = CGImagePropertyOrientation(rawValue: metadataOrientation) else { return nil }
+        let imageOrientation = Image.Orientation(cgImageOrientation)
+        let thumbnailImage = Image(decorative: previewCGImage, scale: 1, orientation: imageOrientation)
+
+        let photoDimensions = photo.resolvedSettings.photoDimensions
+        let imageSize = (width: Int(photoDimensions.width), height: Int(photoDimensions.height))
+        let previewDimensions = photo.resolvedSettings.previewDimensions
+        let thumbnailSize = (width: Int(previewDimensions.width), height: Int(previewDimensions.height))
+
+        return PhotoData(thumbnailImage: thumbnailImage, thumbnailSize: thumbnailSize, imageData: imageData, imageSize: imageSize)
+    }
+
+    func savePhoto(imageData: Data) {
+        Task {
+            do {
+                try await photoCollection.addImage(imageData)
+                logger.debug("Added image data to photo collection.")
+            } catch let error {
+                logger.error("Failed to add image to photo collection: \(error.localizedDescription)")
             }
         }
     }
