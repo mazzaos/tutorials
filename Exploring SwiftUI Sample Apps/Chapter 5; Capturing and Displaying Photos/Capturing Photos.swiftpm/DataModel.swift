fb21a0caea9f941f9823bf28086d8653badf1cc3
Lorenzo Mazzarotto <mazzaos@icloud.com>
2022-06-02 08:55:12 +0200

    Using the Camera
    Learn about the camera in your data model.

---
+++ Exploring SwiftUI Sample Apps/Chapter 5; Capturing and Displaying Photos/Capturing Photos.swiftpm/DataModel.swift
@@ -0,0 +1,83 @@
+//
+//  DataModel.swift
+//  Capturing Photos
+//
+//  Created by Lorenzo Lins Mazzarotto on 02/06/22.
+//
+
+import AVFoundation
+import SwiftUI
+import os.log
+
+final class DataModel: ObservableObject {
+    let camera = Camera()
+
+    @Published var viewfinderImage: Image?
+    @Published var thumbnailImage: Image?
+
+    var isPhotosLoaded = false
+
+    func loadPhotos() async {
+        guard !isPhotosLoaded else { return }
+
+        let authorized = await PhotoLibrary.checkAuthorization()
+        guard authorized else {
+            logger.error("Photo library access was not authorized.")
+            return
+        }
+
+        Task {
+            do {
+                try await self.photoCollection.load()
+                await self.loadThumbnail()
+            } catch let error {
+                logger.error("Failed to load photo collection: \(error.localizedDescription)")
+            }
+            self.isPhotosLoaded = true
+        }
+    }
+
+    func loadThumbnail() async {
+        guard let asset = photoCollection.photoAssets.first  else { return }
+        await photoCollection.cache.requestImage(for: asset, targetSize: CGSize(width: 256, height: 256))  { result in
+            if let result = result {
+                Task { @MainActor in
+                    self.thumbnailImage = result.image
+                }
+            }
+        }
+    }
+}
+
+fileprivate struct PhotoData {
+    var thumbnailImage: Image
+    var thumbnailSize: (width: Int, height: Int)
+    var imageData: Data
+    var imageSize: (width: Int, height: Int)
+}
+
+fileprivate extension CIImage {
+    var image: Image? {
+        let ciContext = CIContext()
+        guard let cgImage = ciContext.createCGImage(self, from: self.extent) else { return nil }
+        return Image(decorative: cgImage, scale: 1, orientation: .up)
+    }
+}
+
+fileprivate extension Image.Orientation {
+
+    init(_ cgImageOrientation: CGImagePropertyOrientation) {
+        switch cgImageOrientation {
+        case .up: self = .up
+        case .upMirrored: self = .upMirrored
+        case .down: self = .down
+        case .downMirrored: self = .downMirrored
+        case .left: self = .left
+        case .leftMirrored: self = .leftMirrored
+        case .right: self = .right
+        case .rightMirrored: self = .rightMirrored
+        }
+    }
+}
+
+fileprivate let logger = Logger(subsystem: "com.apple.swiftplaygroundscontent.capturingphotos", category: "DataModel")
