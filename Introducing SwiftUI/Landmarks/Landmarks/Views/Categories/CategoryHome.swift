0d48a5c5a0d58cbc40caf0e86ae94f2722cab9a5
Lorenzo Mazzarotto <mazzaos@icloud.com>
2022-06-05 17:31:07 +0200

    Add a Category View
    You can provide a different way to browse the landmarks by creating a view that sorts landmarks by category, while highlighting a featured landmark at the top of the view.

---
+++ Introducing SwiftUI/Landmarks/Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Lorenzo Lins Mazzarotto on 05/06/22.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello, World!")
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
