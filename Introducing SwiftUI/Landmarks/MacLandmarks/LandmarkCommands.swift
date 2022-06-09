9fc0fde6fcb66b9f326786c065d9cb7895a738b5
Lorenzo Mazzarotto <mazzaos@icloud.com>
2022-06-09 02:11:58 +0200

    Add a Built-in Menu Command
    When you create an app using the SwiftUI life cycle, the system automatically creates a menu with commonly needed items, like those for closing the front-most window or for quitting the app. SwiftUI lets you add other common commands with built-in behavior, as well as completely custom commands.
    
    In this section, you’ll add a system-provided command that lets the user toggle the sidebar, to be able to get it back after dragging it closed.

---
+++ Introducing SwiftUI/Landmarks/MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,14 @@
+//
+//  LandmarkCommands.swift
+//  Landmarks
+//
+//  Created by Lorenzo Lins Mazzarotto on 09/06/22.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
